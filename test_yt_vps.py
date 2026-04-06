a9dacc3c6487814f76913a2ddfd407e1d06627a9
iiiaha <iiiaha@naver.com>
2026-04-06 09:06:25 +0900

    Test mobile responsive email

--- test_yt_vps.py
+++ test_yt_vps.py
@@ -1,6 +1,20 @@
-import json
-data=json.load(open('/opt/idi/data/youtube_2026-04-05.json'))
-for v in data:
-    t='O' if v.get('has_transcript') else 'X'
-    print(f"[{t}] https://youtube.com/watch?v={v['video_id']}")
-    print(f"    {v['channel']} | {v['title'][:50]}")
+import sys,json
+sys.path.insert(0,'/opt/idi')
+from dotenv import load_dotenv
+load_dotenv('/opt/idi/.env')
+from jinja2 import Environment, FileSystemLoader
+from mailer.resend_mailer import ResendMailer
+
+analysis=json.load(open('/opt/idi/data/analysis_2026-04-05.json'))
+yt=None
+try:
+    yt=json.load(open('/opt/idi/data/youtube_analysis_2026-04-05.json'))
+except:
+    pass
+
+env=Environment(loader=FileSystemLoader('/opt/idi/report/templates'),autoescape=True)
+html=env.get_template('daily_v6.html').render(analysis=analysis,meta=analysis['meta'],youtube=yt,report_date='2026-04-05',unsubscribe_url='#')
+open('/opt/idi/data/report_test.html','w').write(html)
+
+m=ResendMailer(api_key='re_jWAT4Yhp_Et3gxfMRfVUtLdPf4PwoVFeR')
+print('sent' if m.send('iiiaha@naver.com','[test] mobile responsive',html) else 'fail')
